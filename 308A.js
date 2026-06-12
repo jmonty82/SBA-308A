6eaf873d58b92f7d0d1ae83a8add543fa792dd3b
jmonty82 <jmontrose82@gmail.com>
2026-06-12 08:40:32 -0400

    Fourth Commit

--- 308A.js
+++ 308A.js
@@ -1,14 +1,29 @@
-async function fetchData(){
+async function getData(){
     try {
-        const search = document.getElementById("Search").value.toLowerCase();
-        const response = await fetch("https://api.jikan.moe/v4/anime")
+        const url = "https://api.jikan.moe/v4/anime/";
+        const response = await fetch(`url/${animeName}`)
+            .then(res => res.json())
+            .then(data => console.log(data));
+        const animeName = document.getElementById("animeName").value.toLowerCase();
         if (!response.ok) {
             throw new Error("Could not fetch resource");
         }
-        const data = await response.json();
-        console.log(data);
+        // const data = await response.json()
+        // console.log(data);
+        const animeImage = data.image
+        const imgElement = document.getElementById("animeImage");
+        imgElement.src = animeImage;
+        imgElement.style.display = "block";
     }
     catch (error) {
         console.error(error);
     }
 }
+getData()
+// get/anime/{id}/full
+// data.images
+
+// const url = "https://api.jikan.moe/v4/anime/";
+// fetch(url)
+// .then(res => res.json())
+// .then(data => console.log(data));
